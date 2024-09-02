91aa01c29d88cd92a7d4918fb3908875278d0c7c
HRNAE <148918405+HRNAE@users.noreply.github.com>
2024-09-02 00:53:02 -0500

    Update densenet.py

--- densenet.py
+++ densenet.py
@@ -190,7 +190,7 @@ optimizer = optim.SGD(net.parameters(), lr=best_lr, momentum=best_momentum)
 # Define loss function and optimizer
 criterion = nn.CrossEntropyLoss()
 
-for epoch in range(5):  # Adjust epoch count as needed
+for epoch in range(3):  # Adjust epoch count as needed
     net.train()
     running_loss = 0.0
     for i, data in enumerate(train_loader, 0):
