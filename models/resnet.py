e0f93788aff1f6350de01dfaeb4397e86e2027d9
HRNAE <148918405+HRNAE@users.noreply.github.com>
2024-09-10 01:49:18 -0500

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -164,7 +164,7 @@ def objective(trial):
     return accuracy
 
 study = optuna.create_study(direction='maximize')
-study.optimize(objective, n_trials=1)
+study.optimize(objective, n_trials=3)
 
 best_params = study.best_params
 print("Best parameters found by Optuna:", best_params)
@@ -176,7 +176,7 @@ optimizer = optim.SGD(net.parameters(), lr=best_lr, momentum=best_momentum)
 
 criterion = nn.CrossEntropyLoss()
 
-for epoch in range(1):  # Adjust epoch count
+for epoch in range(10):  # Adjust epoch count
     net.train()
     running_loss = 0.0
     for i, data in enumerate(train_loader, 0):
